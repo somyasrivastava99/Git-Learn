3b49f33e14da068c84122162f61b18ce8c9258e8
unknown <you@example.com>
2018-03-07 18:33:53 +0530

    made changes

--- person1.py
+++ person1.py
@@ -1,15 +1,8 @@
 
-import random
 
-chars = '''ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789,./;'[]!@#$%^&*()_+~\|:"<>?'''
-times = int(input('[.]Number of passwords?\n[.]'))
-length = int(input('[.]Password length?\n[.]'))
+#for -------
 
-f = open('path/location','w')
+	password = password + '\n'
+	f.write(password)
 
-for i in range(times):
-	password = ''
-	for j in range(length):
-		password += random.choice(chars)
-
-	print('[.]',password,sep='')
+f.close()
